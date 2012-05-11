c5ebdf37bc362e5c6ea3ec92121aa37eddd28d55
Paul Cichonski <paul.cichonski@gmail.com>
2012-05-11 15:41:06 -0400

    initial project structure

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject web-crawler "0.0.1-SNAPSHOT"
+  :description "Simple tool to crawl a single domain (to a specified depth) and store content for offline use."
+  :dependencies [
+                 [org.clojure/clojure "1.4.0"]
+                 ]
+  :dev-dependencies [
+                     [com.github.robertrolandorg/lein-eclipse "1.1.0"]                     
+                     ]    
+            )
