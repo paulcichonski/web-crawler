1d01cc1579466647a65bebdea70c54ca70a0e738
Paul Cichonski <paul.cichonski@gmail.com>
2012-05-12 22:29:24 -0400

    start of some tests

---
+++ test/cichonski/test/crawler.clj
@@ -0,0 +1,8 @@
+(ns cichonski.test.crawler
+    ^{:author "Paul Cichonski"
+    :doc "tests.
+           work-in-progress"}
+    (:use [cichonski.crawler]))
+
+(comment
+(crawl-domain "http://test.com"))
