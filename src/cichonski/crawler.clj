955fbe632fb8ca346510ac49c2cd9da35cf60331
Paul Cichonski <paul.cichonski@gmail.com>
2012-05-12 20:35:55 -0400

    initial recursion of children pages is working, only goes through one link right now though (bit of a hack, but will soon fix

--- src/cichonski/crawler.clj
+++ src/cichonski/crawler.clj
@@ -4,7 +4,9 @@
 
            work-in-progress"}
     (:use [clojure.string :only (split)])
-    (:require [net.cgrand.enlive-html :as enlive]))
+    (:require [net.cgrand.enlive-html :as enlive]
+              [clojurewerkz.urly.core :as urly])
+    (:import [clojurewerkz.urly UrlLike]))
 
 (comment "the follow-redirect logic works, but it still doesn't handle everything, clojure.org has a redirect scheme to wiki.sessions to track users,
 and then back to clojure.org with a session token...it traveerses the links but still gets nothing back. TODO: fix")
@@ -23,24 +25,16 @@ and then back to clojure.org with a session token...it traveerses the links but 
                                   (recur new-url new-conn new-response)))))]
   (enlive/html-resource (follow-redirects url))))
 
-(defn- parse-domain [url]
-  "parse the domain. TODO: this is currently really naive, improve!"
-  ;; use "www.google.com as an example, want to return the google.com part, note: this could also be video.google.com
-  ;; break out the "x.google.x" part, which is returend from the regex and concat second two elements
-  (try 
-    (let [domain-components (split (second (re-find #"^(?:\w*://)?([^/?#]+)?(.*?)" url)) #"\." )]
-      (cond (= (count domain-components) 3) (str (second domain-components) "." (nth domain-components 2))
-            (= (count domain-components) 2) (str (first domain-components) "." (second domain-components))))
-    ;; TODO: try/catch is here for debugging, need to remove.
-    (catch Exception e (throw (Exception. (str "could not find domain of URL: " url (.getMessage e)))))))
 
-(comment "TODO: use this: https://github.com/michaelklishin/urly for doing this logic")
-(defn- local-link? [target domain]
-  "ensure that the link is to something in this domain"
-  (cond (= (.charAt target 0) \/) true
-        (= (parse-domain target) domain) true
-        (not= (parse-domain target) domain) nil
-        :else (throw (Exception. (str "not able to detrmine if target link is local, need new case for: " target)))))
+
+(defn- local-link? [^String target ^String parent]
+  "ensure that the link is in the same domain as the parent node (i.e., doesn't point to different domain)"
+  (let [target (urly/url-like target)
+        parent (urly/url-like parent)]
+    (cond (false? (urly/absolute? target)) true
+          (= (urly/host-of target) (urly/host-of target)) true
+          (not= (urly/host-of target) (urly/host-of target)) nil
+          :else (throw (Exception. (str "not able to detrmine if target link is local, need new case for: " target))))))
 
 (defn- store-page [html-map file-loc]
   (spit file-loc (apply str (enlive/emit* html-map))))
@@ -56,18 +50,25 @@ rely on all html being valid xml.....actually it seems like enlive returns a val
      depth -> how deep into the page-graph to crawl (each linked page is a node)
      directory -> where to store the files
      pred -> every html element in start will be run through pred, if false that node and all children will be ignored"
-    (let [domain (parse-domain start)
+    (let [grab-domain-links (fn [all-links] 
+                              (filter #(local-link? ((% :attrs) :href) start) all-links))
+          start-urly (urly/url-like start)
           root-page (fetch-page start)
-          links (enlive/select root-page [:a])
-          domain-links (filter #(local-link? ((% :attrs) :href) domain) links)]
+          domain-links (grab-domain-links (enlive/select root-page [:a]))]
       (store-page root-page (str directory "root.html"))
-      (loop [link (((first domain-links) :attrs) :href)
+      (loop [link (((first domain-links) :attrs) :href) ;; HACK to test, need to go throuh entire list!
              dp depth]
-        (if (> dp 1) 
-          (store-page (fetch-page link) "root/link1.html")
-          (recur (first (filter #(local-link? ((% :attrs) :href) domain) (enlive/select link [:a])))
-                 (dec dp)))))))
+        (let [valid-link (.toURL (urly/resolve start-urly (urly/url-like link)))]
+          (if (> dp 1) 
+                (store-page (fetch-page link) "root/link1.html")
+                (recur (first (grab-domain-links (enlive/select link [:a]))) ;; HACK to test, need to go throuh entire list!
+                       (dec dp))))))))
 
 
 
 
+(comment "todays goals:
+          1) write code for storing pages
+          2) write code for recursing through link structure to the requested depth
+          3) write code for ensuring all links to children-pages are made local
+          4) store images")
